7dd0aeac997c84a3a5f440fa1f6613dac2a9e023
Evan Stites-Clayton <stitesclayton@gmail.com>
2022-12-04 09:21:59 -0600

    Fix the off by one pixel error

--- c/EmailForm.tsx
+++ c/EmailForm.tsx
@@ -9,45 +9,55 @@ const EmailForm: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
+      <div
         style={{
-          padding: "12px 12px",
-          borderTopRightRadius: 0,
-          borderBottomRightRadius: 0,
-          borderTopLeftRadius: 8,
-          borderBottomLeftRadius: 8,
-          border: "none",
-          backgroundColor: "#F9F7F5",
-          width: "40vw",
-          maxWidth: 200,
-        }}
-        id="email"
-        type="email"
-        name="email"
-        placeholder="me@mail.com"
-      />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <button
-        type="submit"
-        disabled={state.submitting}
-        onClick={() => {}}
-        style={{
-          backgroundColor: "#78C1DD",
-          fontFamily: "Inter",
-          fontSize: 12,
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "row",
           marginTop: 24,
-          borderTopRightRadius: 8,
-          borderBottomRightRadius: 8,
-          color: "black",
-          borderWidth: 0,
-          padding: "12px 12px",
-          cursor: "pointer",
-          width: "45vw",
-          maxWidth: 200,
         }}
       >
-        stay updated
-      </button>
+        <input
+          style={{
+            padding: "12px 12px",
+            height: 40,
+            borderTopRightRadius: 0,
+            borderBottomRightRadius: 0,
+            borderTopLeftRadius: 8,
+            borderBottomLeftRadius: 8,
+            border: "none",
+            backgroundColor: "#F9F7F5",
+            width: "40vw",
+            maxWidth: 200,
+          }}
+          id="email"
+          type="email"
+          name="email"
+          placeholder="me@mail.com"
+        />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <button
+          type="submit"
+          disabled={state.submitting}
+          onClick={() => {}}
+          style={{
+            backgroundColor: "#78C1DD",
+            fontFamily: "Inter",
+            fontSize: 12,
+            height: 40,
+            borderTopRightRadius: 8,
+            borderBottomRightRadius: 8,
+            color: "black",
+            borderWidth: 0,
+            padding: "12px 12px",
+            cursor: "pointer",
+            width: "45vw",
+            maxWidth: 200,
+          }}
+        >
+          stay updated
+        </button>
+      </div>
     </form>
   );
 };
